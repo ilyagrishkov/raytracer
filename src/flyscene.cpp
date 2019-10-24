de4b5dc1ed97fd0604ccf870b4c3c815bd008616
jerry987m <jerry987m@gmail.com>
2019-10-24 20:51:36 +0200

    Barebones Diffuse

--- src/flyscene.cpp
+++ src/flyscene.cpp
@@ -1,5 +1,7 @@
 #include "flyscene.hpp"
 #include <GLFW/glfw3.h>
+#include "boundingbox.hpp"
+#include"boundingbox.cpp"
 
 
 
--- src/flyscene.hpp
+++ src/flyscene.hpp
@@ -16,172 +16,16 @@
 #include <tucano/utils/mtlIO.hpp>
 #include <tucano/utils/objimporter.hpp>
 #include <float.h>
+#include "boundingbox.hpp"
+#include <chrono>
 
 
-static float RAYLENGTH = 10.0;
-static const int MAX_DEPTH = 5;
-
-struct vectorTwo {
-	float x;
-	float y;
-
-	vectorTwo operator/ (float divisor) {
-		vectorTwo out;
-		out.x = x / divisor;
-		out.y = y / divisor;
-		return out;
-	}
-
-	vectorTwo operator- (vectorTwo other) {
-		vectorTwo out;
-		out.x = x - other.x;
-		out.y = y - other.y;
-		return out;
-	}
-
-	vectorTwo operator+ (vectorTwo other) {
-		vectorTwo out;
-		out.x = x + other.x;
-		out.y = y + other.y;
-		return out;
-	}
-
-	vectorTwo operator* (vectorTwo other) {
-		vectorTwo out;
-		out.x = other.x * x;
-		out.y = other.y * y;
-		return out;
-	}
-
-	vectorTwo operator* (float other) {
-		vectorTwo out;
-		out.x = other * x;
-		out.y = other * y;
-		return out;
-	}
-
-	float length() {
-		return sqrt(x * x + y * y);
-	}
-};
-
-struct vectorThree {
-	float x;
-	float y;
-	float z;
-
-	vectorThree operator/ (float divisor) {
-		vectorThree out;
-		out.x = x / divisor;
-		out.y = y / divisor;
-		out.z = z / divisor;
-		return out;
-	}
-
-	vectorThree operator- (vectorThree other) {
-		vectorThree out;
-		out.x = x - other.x;
-		out.y = y - other.y;
-		out.z = z - other.z;
-		return out;
-	}
-
-	vectorThree operator+ (vectorThree other) {
-		vectorThree out;
-		out.x = x + other.x;
-		out.y = y + other.y;
-		out.z = z + other.z;
-		return out;
-	}
-
-	vectorThree operator* (vectorThree other) {
-		vectorThree out;
-		out.x = other.x * x;
-		out.y = other.y * y;
-		out.z = other.z * z;
-		return out;
-	}
-
-	vectorThree operator* (float other) {
-		vectorThree out;
-		out.x = other * x;
-		out.y = other * y;
-		out.z = other * z;
-		return out;
-	}
-
-	vectorThree operator* (float other) const {
-		vectorThree out;
-		out.x = other * x;
-		out.y = other * y;
-		out.z = other * z;
-		return out;
-	}
-
-	
-
-	float dot(vectorThree other) {
-		float result = 0;
-		result += x * other.x;
-		result += y * other.y;
-		result += z * other.z;
-		return result;
-	}
-
-	vectorThree cross(vectorThree other) {
-		vectorThree result;
-		result.x = (y * other.z) - (z * other.y);
-		result.y = (z * other.x) - (x * other.z);
-		result.z = (x * other.y) - (y * other.x);
-		return result;
-	}
-
-	float scalarTripleProduct(vectorThree v, vectorThree w) {
-		return cross(v).dot(w);
-	}
-
-	float length() {
-		return sqrt(x * x + y * y + z * z);
-	}
-
-	static vectorThree toVectorThree(Eigen::Vector3f old) {
-		vectorThree out = { old(0), old(1), old(2) };
-		return out;
-	}
-
-};
-
-struct face {
-	vectorThree vertex1;
-	vectorThree vertex2;
-	vectorThree vertex3;
-	vectorThree normal;
-	int material_id;
-};
-
-struct boundingBox {
-	float xMax;
-	float xMin;
-	float yMax;
-	float yMin;
-	float zMax;
-	float zMin;
-	std::vector<boundingBox> children;
-	std::vector<face> faces;
-
-	boundingBox() {
-		xMax = -FLT_MAX;
-		xMin = FLT_MAX;
-		yMax = -FLT_MAX;
-		yMin = FLT_MAX;
-		zMax = -FLT_MAX;
-		zMin = FLT_MAX;
-	}
-};
 
 class Flyscene {
 
 public:
+
+
   Flyscene(void) {}
 
   /**
@@ -230,7 +74,9 @@ public:
    * @param dest Other point on the ray, usually screen coordinates
    * @return a RGB color
    */
-  Eigen::Vector3f traceRay(vectorThree &origin, vectorThree &dest, std::vector<boundingBox> &boxes, float &lengthRay=RAYLENGTH);
+
+  Eigen::Vector3f traceRay(vectorThree &origin, vectorThree &dest, std::vector<BoundingBox> &boxes, float &lengthRay=RAYLENGTH);
+
   
 private:
   // A simple phong shader for rendering meshes
