34b8087dec163595a77adee1dffce4191f64577b
Gancho <ganchosta@gmail.com>
2019-10-27 20:33:19 +0100

    Debugray in progress 2

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,8 @@
 #include "flyscene.hpp"
 #include <iostream>
 
-#define WINDOW_WIDTH 1000
-#define WINDOW_HEIGHT 1000
+#define WINDOW_WIDTH 300
+#define WINDOW_HEIGHT 300
 
 Flyscene *flyscene;
 Eigen::Vector2f mouse_pos = Eigen::Vector2f::Zero();
